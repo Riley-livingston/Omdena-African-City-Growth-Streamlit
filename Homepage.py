5441d06f9f109d3bac2b4c90156a3e7ddba7a99e
Riley Livingston <rdlivin@bgsu.edu>
2023-05-06 14:18:14 -0400

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -15,3 +15,29 @@ st.markdown(
 )
 
 st.sidebar.success("Select a dashboard above.")
+
+
+# Buttons to navigate to different pages
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write("")
+with col2:
+    if st.button("Dashboard 1"):
+        st.experimental_set_query_params(dashboard="dashboard_1")
+    if st.button("Dashboard 2"):
+        st.experimental_set_query_params(dashboard="dashboard_2")
+with col3:
+    st.write("")
+
+query_params = st.experimental_get_query_params()
+
+# Navigation logic based on the buttons clicked
+if "dashboard" in query_params:
+    if query_params["dashboard"][0] == "dashboard_1":
+        
+        st.write("This is Dashboard 1")
+    elif query_params["dashboard"][0] == "dashboard_2":
+       
+        st.write("This is Dashboard 2")
+else:
+    st.write("Select a dashboard to view")
