fe18b581164e6db7996b0ee110760665d8d51b1f
Riley Livingston <rdlivin@bgsu.edu>
2023-05-07 11:56:46 -0400

    Update 🏠_Omdena_Homepage.py

--- 🏠_Omdena_Homepage.py
+++ 🏠_Omdena_Homepage.py
@@ -14,7 +14,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-st.write("""<p style='margin:0in;font-size:15px;font-family:"Helvetica Neue",serif;color:black;border:none;'><span style='font-family:"Arial",sans-serif;'>In this project, we aim to introduce how a group can use our data and research to build a full model that predicts urban growth, city maintenance, and environmental monitoring.&nbsp;</span></p>
+st.markdown("""<p style='margin:0in;font-size:15px;font-family:"Helvetica Neue",serif;color:black;border:none;'><span style='font-family:"Arial",sans-serif;'>In this project, we aim to introduce how a group can use our data and research to build a full model that predicts urban growth, city maintenance, and environmental monitoring.&nbsp;</span></p>
 <p style='margin:0in;font-size:15px;font-family:"Helvetica Neue",serif;color:black;border:none;'><span style='font-family:"Arial",sans-serif;'>&nbsp;</span></p>
 <p style='margin:0in;font-size:15px;font-family:"Helvetica Neue",serif;color:black;border:none;'><span style='font-family:"Arial",sans-serif;'>We have used the United Nations data, referenced in the resources page, to analyze the reliability of the data by assessing the error rates. The error rates are calculated using population census, estimates, and projections. The analysis is broken down in the accuracy analysis page.</span></p>
 <p style='margin:0in;font-size:15px;font-family:"Helvetica Neue",serif;color:black;border:none;'><span style='font-family:"Arial",sans-serif;'>&nbsp;</span></p>
